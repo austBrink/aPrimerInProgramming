0a4dfe624bbeba97525f723f2e1b53fe282fa247
austBrink <austbrink@gmail.com>
2022-09-22 06:02:58 -0500

    recursion, setTimeout and promises

--- phase-one/unit4/teacherNotes.js
+++ phase-one/unit4/teacherNotes.js
@@ -1,24 +1,38 @@
-// recursion example for webdev simplified. 
-function countDown(n) {
-    for (let i = n; i > 0; i--) {
-        console.log(i);
-    }
-    console.log('yo');
+// Set Timeout. A way to wait...
+// Syntax... setTimeout(thingToCall, afterThisLong);
+// const delayedGreeting = setTimeout(() => {console.log("Hello")},5000);
+
+const sleep = (howLongToSleep) => {
+    return new Promise((resolve) => setTimeout(resolve, howLongToSleep));
 }
 
-function countDownRecursive(n) {
-    // first need a break.
-    if (n <= 0) {
-        console.log('yo');
-        return;
-    }
-    console.log(n);
-    countDownRecursive(n-12);
+
+//async ONLY FOR THAT FUNCTION!! 
+const delayedGreeting = async () => {
+    await sleep(5000);
+    console.log("Hello");
 }
 
-// use recursion insread of a loop for arrays!! 
-const recursiveLoop = () => {
-    
+
+// HOW TO DO .then() ????? 
+delayedGreeting();
+
+// what if I want to wait on this. 
+
+
+
+// RECURSION! Another way to loop.
+// Functions
+const recursiveLoop = (array, n) => {
+    if ( n < 0 ) return;
+    console.log(array[n]);
+    recursiveLoop(array, n - 1);
 }
 
-countDown(10);
+const listArray = (array) => {
+    recursiveLoop(array, array.length - 1);
+}
+
+// Usage
+myArray = [6,5,4,3,2,1];
+listArray(myArray);
