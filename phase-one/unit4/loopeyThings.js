ae67e617124ba39e4e06e883a59ef4a59ce8f641
austBrink <austbrink@gmail.com>
2022-09-23 21:50:01 -0500

    fancy that

--- phase-one/unit4/loopeyThings.js
+++ phase-one/unit4/loopeyThings.js
@@ -9,19 +9,27 @@ const listArray = (array) => {
     recursiveLoop(array, array.length - 1);
 }
 
+const line = () => {
+    console.log("=============================================================");
+}
+
 // An array ... 
 myArray = [6,5,4,3,2,1];
 
 // I wish to print this array. 
 console.log(myArray);
-
+line();
 // but if I wish to be fancy as I do in other languages....
 for (i = 0; i < myArray.length; i++) {
     console.log(myArray[i]);
 }
 
+line();
+
 for (const element of myArray) {
     console.log(element);
 }
+
+line();
 
 listArray(myArray);
